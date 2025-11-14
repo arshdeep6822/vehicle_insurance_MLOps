53050f0e049b217689d28936d098b1146695f774
arshdeep6822 <deeparsh618@gmail.com>
2025-11-14 17:02:49 +0530

    project structure and environment ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="arshdeep",
+    author_email="deeparsh618@gmail.com",
+    packages=find_packages()
+)
